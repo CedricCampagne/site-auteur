7bc58b00f7a9d831b3bf86a80a6cfb16422b8612
CedricCampagne <ced.campagne@gmail.com>
2026-01-11 15:53:26 +0100

    feat: initial homepage layout with HTML/CSS and placeholder content

---
+++ client/src/lib/components/Footer.svelte
@@ -0,0 +1,28 @@
+<script>
+  
+</script>
+
+<footer class="flex justify-center bg-accent2 text-white border-t">
+  <nav>
+    <ul class="flex">
+      <li class="p-2">
+        <a href="">Accueil</a>
+      </li>
+      <li class="p-2">
+        <a href="">Livres</a>
+      </li>
+      <li class="p-2">
+        <a href="">Chroniques</a>
+      </li>
+      <li class="p-2">
+        <a href="">A propos</a>
+      </li>
+      <li class="p-2">
+        <a href="">Contact</a>
+      </li>
+      <li class="p-2">
+        <a href="">Mentions Légales</a>
+      </li>
+    </ul>
+  </nav>
+</footer>
---
+++ client/src/lib/components/LastChroniques.svelte
@@ -0,0 +1,15 @@
+<script>
+	import BookCard from "./BookCard.svelte";
+	import ChroniqueCard from "./ChroniqueCard.svelte";
+
+
+</script>
+
+<section class="bg-bg-main p-6">
+    <h2 class="text-title text-2xl mb-2">Dernières chroniques</h2>
+    <div class="flex gap-3">
+       <ChroniqueCard/>
+       <ChroniqueCard/>
+       <ChroniqueCard/>
+    </div>
+</section>
