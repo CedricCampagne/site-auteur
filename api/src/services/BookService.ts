daa044d609bb944c70cfc5d308c294fb97ff19f0
CedricCampagne <ced.campagne@gmail.com>
2026-01-12 17:11:25 +0100

    feat(books): add 3 new routes (getAll, getRandom3, getLatest) with controllers and service integration

---
+++ api/src/services/BookService.ts
@@ -0,0 +1,28 @@
+import { Book } from "../models/Book";
+import { sequelize } from "../config/database";
+
+export class BookService {
+    static async getAllBooks() {
+        return Book.findAll();
+    }
+
+    static async getRandom3Books() {
+        return Book.findAll({
+            order: sequelize.literal('RANDOM()'),
+            limit: 3
+        })
+    }
+
+    static async getLatestBooks() {
+        return Book.findAll({
+            order: [["published_at", "DESC"]],
+            limit: 1
+        })
+    }
+
+    static async getBySlug(slug: string) {
+        return Book.findOne({
+           where: { slug }
+        });
+    }
+}
