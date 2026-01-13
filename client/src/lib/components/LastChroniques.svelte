147f52fecc2014dcb279f929fcbd1867090bd7a3
CedricCampagne <ced.campagne@gmail.com>
2026-01-13 18:42:20 +0100

    feat: display latest Chronicle data on home page

--- client/src/lib/components/LastChroniques.svelte
+++ client/src/lib/components/LastChroniques.svelte
@@ -1,15 +1,17 @@
-<script>
-	import BookCard from "./BookCard.svelte";
+<script lang="ts">
+	import type { Chronicle } from "$lib/types";
 	import ChroniqueCard from "./ChroniqueCard.svelte";
 
+    export let latest3Chronicles : Chronicle[];
 
 </script>
 
 <section class="bg-bg-main p-6 mt-8 mx-15 flex flex-col gap-6 border-b border-accent2">
     <h2 class="text-title text-2xl mb-2">Dernières chroniques</h2>
-    <div class="flex gap-3">
-       <ChroniqueCard/>
-       <ChroniqueCard/>
-       <ChroniqueCard/>
+     <div class="flex gap-3 mt-2">
+        {#each latest3Chronicles as chronicle }
+        <ChroniqueCard { chronicle}/>            
+        {/each}
+      
     </div>
 </section>
--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -4,12 +4,14 @@
 	import HeroSection from "$lib/components/HeroSection.svelte";
 	import LastBooks from "$lib/components/LastBooks.svelte";
 	import LastChroniques from "$lib/components/LastChroniques.svelte";
+	
 
 	export let data;
 	
 	// const { latestBook } = data;
 	const latestbook = data.latestbook;
 	const allbooks = data.allbooks;
+	const latest3Chronicles = data.latest3Chronicles;
 
 </script>
 
@@ -17,5 +19,5 @@
 <HeroSection />
 <LastBooks { latestbook } />
 <Bibliography { allbooks }/>
-<LastChroniques/>
+<LastChroniques { latest3Chronicles} />
 <About/>
