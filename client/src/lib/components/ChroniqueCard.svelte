d15bebe87272bf9e5c23255c539dcd66ea46ef2e
CedricCampagne <ced.campagne@gmail.com>
2026-01-14 16:46:49 +0100

    feat(home): complete CSS and responsive design down to 320px

--- client/src/lib/components/ChroniqueCard.svelte
+++ client/src/lib/components/ChroniqueCard.svelte
@@ -5,7 +5,7 @@
   export let chronicle: Chronicle;
 </script>
 
-<article class="flex flex-col h-112 overflow-hidden bg-white">
+<article class="flex flex-col overflow-hidden bg-white shadow-lg shadow-accent2 h-112 w-3/4">
   <!-- IMAGE verrouillée -->
   <!-- basis-1/3 prend exactement 33% de la hauteur du <article> -->
   <div class="relative basis-1/3 shrink-0">
@@ -23,7 +23,7 @@
     <h3 class="font-title text-lg line-clamp-2">{chronicle.title}</h3>
     <p class="text-accent1 line-clamp-2">{chronicle.quote}</p>
     <p class="text-sm text-text-soft line-clamp-2">{chronicle.summary}</p>
-    <Button text="Lire la chronique" className="bg-accent1 text-white text-sm" />
+    <Button text="Lire la chronique" className="text-sm border-2 bg-accent1 text-white hover:bg-white hover:text-accent1 transition-all duration-500" />
   </div>
 </article>
 
--- client/src/lib/components/LastChroniques.svelte
+++ client/src/lib/components/LastChroniques.svelte
@@ -6,24 +6,11 @@
 
 </script>
 
-<section class="flex flex-col gap-4 mt-12 pb-4 border-b">
-    <h2 class="text-title text-2xl mb-2">Dernières chroniques</h2>
-     <div class="flex gap-4">
+<section class="flex flex-col  gap-4 mt-12 pb-8 border-b">
+    <h2 class="text-center md:text-start text-title text-2xl">Dernières chroniques</h2>
+     <div class="flex flex-col items-center gap-4 md:flex md:flex-row">
         {#each latest3Chronicles as chronicle }
         <ChroniqueCard { chronicle}/>            
         {/each}
-      
     </div>
 </section>
-
-
-
-<!-- <section class="bg-bg-main p-6 mt-8 mx-15 flex flex-col gap-6 border-b border-accent2">
-    <h2 class="text-title text-2xl mb-2">Dernières chroniques</h2>
-     <div class="flex gap-3 mt-2">
-        {#each latest3Chronicles as chronicle }
-        <ChroniqueCard { chronicle}/>            
-        {/each}
-      
-    </div>
-</section> -->
