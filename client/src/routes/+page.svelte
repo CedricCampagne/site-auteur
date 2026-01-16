ca3483a3125566626a5fc037c0992c0872f9657a
CedricCampagne <ced.campagne@gmail.com>
2026-01-16 10:53:06 +0100

    feat: implement book excerpt modal with open/close events and truncated text

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,21 +1,32 @@
 <script>
 	import About from "$lib/components/About.svelte";
 	import Bibliography from "$lib/components/Bibliography.svelte";
+	import BookExcerptModal from "$lib/components/BookExcerptModal.svelte";
 	import HeroSection from "$lib/components/HeroSection.svelte";
 	import LastBooks from "$lib/components/LastBooks.svelte";
 	import LastChroniques from "$lib/components/LastChroniques.svelte";
-	
+
 	export let data;
-	
 	// const { latestBook } = data;
 	const latestbook = data.latestbook;
 	const allbooks = data.allbooks;
 	const latest3Chronicles = data.latest3Chronicles;
-
+	
+	let showModal = false;
 </script>
 
 <HeroSection/>
-<LastBooks { latestbook }/>
+<LastBooks
+	{ latestbook }
+	on:openExcerpt={()=>showModal = true }
+/>
 <Bibliography { allbooks }/>
 <LastChroniques { latest3Chronicles}/>
 <About/>
+
+<BookExcerptModal
+open={showModal}
+text={latestbook.excerpt}
+title={latestbook.title}
+on:close={()=> showModal = false }
+/>
