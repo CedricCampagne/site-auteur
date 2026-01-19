4a18d0a6882589f2a62dce3b6b8f162b7d2ec4fb
CedricCampagne <ced.campagne@gmail.com>
2026-01-19 18:26:28 +0100

    feat(front): add chronicles list page, chronicle details route /chronicles/[id]/[slug] and ChroniqueMainCard component

---
+++ client/src/routes/chronicles/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import ChroniclesMainCard from '$lib/components/ChroniclesMainCard.svelte';
+
+    export let data;
+
+    const chronicles = data.chronicles;
+
+</script>
+
+<section class="flex flex-col gap-4 mt-24 pb-8 border-b">
+    <h2 class="text-5xl font-black text-center mb-4">Chroniques</h2>
+    <h3 class="text-center mb-16">Les dernières  réflexions de Katia</h3>
+    {#each chronicles as chronicle}
+        <ChroniclesMainCard { chronicle } />
+    {/each}
+</section>
