4f5af0c0061b17d6fdd71c6f307ea438a4724468
CedricCampagne <ced.campagne@gmail.com>
2026-01-20 15:04:13 +0100

    feat(front): about page first layout, responsive down to 320px

---
+++ client/src/routes/about/+page.svelte
@@ -0,0 +1,96 @@
+<script lang="ts">
+
+</script>
+
+<section class="flex flex-col gap-4 mt-24 pb-8 border-b">
+    <div>
+        <h2 class="text-5xl font-black text-center mb-4">
+            À propos
+        </h2>
+        <h3 class="text-center mb-16">
+            Quelques mots sur mon parcours et ma vision.
+        </h3>
+    </div>
+
+    <div class="mb-16">
+        <p class="text-lg leading-relaxed text-text-main">
+            Je suis un passionné de récits, d’histoires et de mots. À travers ce site, 
+            je partage mes chroniques, mes réflexions et mes découvertes littéraires. 
+            Mon objectif est simple : transmettre, inspirer et inviter à la lecture.
+
+                 Je suis un passionné de récits, d’histoires et de mots. À travers ce site, 
+            je partage mes chroniques, mes réflexions et mes découvertes littéraires. 
+            Mon objectif est simple : transmettre, inspirer et inviter à la lecture
+        </p>
+    </div>
+
+    <div class="flex flex-col md:flex-row gap-8 md:items-start mb-20">
+        <div class="w-full md:w-1/3 md:self-auto">
+           <div class="w-full aspect-square rounded-lg overflow-hidden shadow">
+                <img 
+                    src="/images/cv_photo.jpeg" 
+                    alt="Portrait" 
+                    class="w-full h-full object-cover"
+                >
+            </div>
+        </div>
+        <div class="w-full md:w-2/3 space-y-2">
+            <h2 class="text-3xl font-title text-title mb-4">Mon histoire</h2>
+            <p class="text-lg leading-relaxed text-text-main">
+                Depuis toujours, j’explore les univers littéraires. Ce site est né de 
+                l’envie de partager mes lectures, mes analyses et mes coups de cœur. 
+                J’aime comprendre ce qui fait vibrer un texte, ce qui le rend unique.
+            </p>
+            <p class="text-lg leading-relaxed text-text-main">
+                Au fil des années, j’ai développé une approche personnelle de la critique 
+                littéraire : sensible, structurée et tournée vers l’émotion.
+            </p>
+           <p class="text-lg leading-relaxed text-text-main">
+                Depuis toujours, j’explore les univers littéraires. Ce site est né de 
+                l’envie de partager mes lectures, mes analyses et mes coups de cœur. 
+                J’aime comprendre ce qui fait vibrer un texte, ce qui le rend unique.
+            </p>
+           <p class="text-lg leading-relaxed text-text-main">
+                Depuis toujours, j’explore les univers littéraires. Ce site est né de 
+                l’envie de partager mes lectures, mes analyses et mes coups de cœur. 
+                J’aime comprendre ce qui fait vibrer un texte, ce qui le rend unique.
+            </p>
+            <p class="text-lg leading-relaxed text-text-main">
+                Depuis toujours, j’explore les univers littéraires. Ce site est né de 
+                l’envie de partager mes lectures, mes analyses et mes coups de cœur. 
+                J’aime comprendre ce qui fait vibrer un texte, ce qui le rend unique.
+            </p>
+            <p class="text-lg leading-relaxed text-text-main">
+                Depuis toujours, j’explore les univers littéraires. Ce site est né de 
+                l’envie de partager mes lectures, mes analyses et mes coups de cœur. 
+                J’aime comprendre ce qui fait vibrer un texte, ce qui le rend unique.
+            </p>
+            
+        </div>
+    </div>
+
+    <div class="flex flex-col gap-8 mb-20">
+        <h2 class="text-3xl font-title text-title mb-4">Ma vision</h2>
+       <p class="text-lg leading-relaxed text-text-main">
+            La littérature est un espace de liberté. Elle permet de voyager, de réfléchir, 
+            de ressentir. À travers mes chroniques, je cherche à mettre en lumière ce qui 
+            rend chaque œuvre singulière.
+        </p>
+    </div>
+
+    <div class="flex flex-col items-center sm:flex-row sm:justify-center gap-8">
+            <a 
+                href="/bibliography" 
+                class="p-2 border bg-accent1 text-white hover:bg-bg-main hover:text-accent1 transition-all duration-500"
+            >
+                Découvrir mes Livres
+            </a>
+      
+            <a 
+                href="/chronicles" 
+                class="p-2 border bg-accent2 text-white hover:bg-bg-main hover:text-accent2 transition-all duration-500"
+            >
+                Découvrir mes chroniques
+            </a>      
+    </div>
+</section>
