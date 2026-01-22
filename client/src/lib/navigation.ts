f0d837bde671b478b5a3d1a65f8a3830dfaa28e0
CedricCampagne <ced.campagne@gmail.com>
2026-01-22 11:41:08 +0100

    feat(header): add Instagram icon with dynamic URL based on device detection

--- client/src/lib/navigation.ts
+++ client/src/lib/navigation.ts
@@ -8,3 +8,27 @@ export function getChronicleUrl(chronicle:Chronicle) {
     console.log(`/chronicles/${chronicle.id_chronicle}/${chronicle.slug}`);
     return `/chronicles/${chronicle.id_chronicle}/${chronicle.slug}`;
 }
+
+export function getDeviceType() {
+        const ua = navigator.userAgent;
+
+        if (/iPad|Tablet|PlayBook|Silk/i.test(ua)) {
+            return "tablet";
+        }
+
+        if (/Mobile|iPhone|Android/i.test(ua)) {
+            return "mobile";
+        }
+
+        return "desktop";
+    }
+
+export function getInstagramUrl(username: string) {
+    const device = getDeviceType();
+
+    if(device === "mobile" || device === "tablet") {
+        return `instagram://user?username=${username}`;
+    }
+
+    return `https://www.instagram.com/${username}/`
+}
