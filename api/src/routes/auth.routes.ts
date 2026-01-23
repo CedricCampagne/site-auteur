ffb3138ca70ec10e4db76c5da4d8b4ff2cdab80d
CedricCampagne <ced.campagne@gmail.com>
2026-01-23 10:29:37 +0100

    feat(api): implement and connect API /auth routes, validate controllers and services through Postman testing

--- api/src/routes/auth.routes.ts
+++ api/src/routes/auth.routes.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
-import { expression } from "joi";
+import { AuthController } from "../controllers/AuthController";
 
 const authRouter = Router();
+
+authRouter.post("/register", AuthController.register);
+authRouter.post("/login", AuthController.login);
 
 export default authRouter;
 
--- api/src/validators/auth/login.schema.ts
+++ api/src/validators/auth/login.schema.ts
@@ -2,5 +2,7 @@ import Joi from "joi";
 
 export const loginSchema = Joi.object({
     email: Joi.string().required(),
-    password: Joi.string().min(6).max(200).required()
+    password: Joi.string().required()
+}).messages({
+     "any.required": "Tous les champs sont obligatoires"
 })
