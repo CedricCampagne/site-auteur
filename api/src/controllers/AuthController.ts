b6f34905a9b801c03ffbe14e52b87f96d4bbc109
CedricCampagne <ced.campagne@gmail.com>
2026-01-23 09:23:48 +0100

    feat(auth): add login to AuthController and login schema validation

--- api/src/controllers/AuthController.ts
+++ api/src/controllers/AuthController.ts
@@ -1,8 +1,9 @@
 import Joi from "joi";
 import { AuthServices } from "../services/authService";
 import { Request, Response } from "express";
+import { LoginParams, RegisterParams } from "../types/auth";
 import { registerSchema } from "../validators/auth/register.schema";
-import { RegisterParams } from "../types/auth";
+import { loginSchema } from "../validators/auth/login.schema";
 
 export class AuthController {
     static async register(req: Request, res: Response) {
@@ -13,14 +14,32 @@ export class AuthController {
                 registerSchema,
                 { stripUnknown: true}   // Pour enlever les champs inconnus par rappoort au schema
                 );
+
             console.log('req body', req.body);
-
             const result = await AuthServices.registerUser(data);
-            console.log("result", result);
 
+            console.log("result", result);
             return res.status(201).json(result);
         } catch (error: any) {
-            res.status(400).json({message : error.message});
+            return res.status(400).json({message : error.message});
+        }
+    }
+
+    static async login(req: Request, res: Response) {
+        try {
+            const data: LoginParams = Joi.attempt(
+                req.body,
+                loginSchema,
+                { stripUnknown: true}
+            );
+
+            console.log('req body', req.body);
+            const result = await AuthServices.loginUser(data);
+
+            console.log("result", result);
+            return res.status(200).json(result);
+        } catch (error: any) {
+            return res.status(400).json({message : error.message});
         }
     }
 }
--- api/src/validators/auth/login.schema.ts
+++ api/src/validators/auth/login.schema.ts
@@ -1,9 +1,6 @@
 import Joi from "joi";
 
-export const registerSchema = Joi.object({
-    username: Joi.string().min(2).max(50).required(),
+export const loginSchema = Joi.object({
     email: Joi.string().required(),
     password: Joi.string().min(6).max(200).required()
 })
-
-
