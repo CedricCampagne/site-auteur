b246df75cfe5e605e255478c27af73b1369c797d
CedricCampagne <ced.campagne@gmail.com>
2026-01-23 13:05:26 +0100

    refactor(books): centralize error handling in BookService and simplify BooksController using global error middleware

--- api/src/services/BookService.ts
+++ api/src/services/BookService.ts
@@ -3,32 +3,59 @@ import { sequelize } from "../config/database";
 
 export class BookService {
     static async getAllBooks() {
-        return Book.findAll({
+        const books = await Book.findAll({
             order : [["published_at", "DESC"]],
             limit: 5
         });
-    }
 
+        if(!books || books.length === 0) {
+            throw { status: 404, message: "Auccun livre trouvé" };
+        }
+
+        return books;
+    }
+   
     static async getRandom3Books() {
-        return Book.findAll({
+        const books = await Book.findAll({
             order: sequelize.literal("RANDOM()"),
             limit: 3
         })
+
+        if(!books || books.length === 0) {
+            throw { status: 404, message: "Auccun livre disponible" };
+        }
+
+        return books;
     }
 
     static async getLatestBook() {
-        return Book.findOne({
+        const book = await Book.findOne({
             order: [["published_at", "DESC"]],
         })
+
+        if (!book) {
+            throw { status: 404, message: "Aucun livre récent trouvé" };
+        }
+        return book;
     }
 
     static async getBySlug(slug: string) {
-        return Book.findOne({
+        const book = await Book.findOne({
            where: { slug }
         });
+
+        if (!book) {
+            throw { status: 404, message: "Aucun livre trouvé avec ce slug" };
+        }
+        return book;
     }
 
     static async getBookById(id:number) {
-        return Book.findByPk(id);
+        const book = await Book.findByPk(id);
+
+        if (!book) {
+            throw { status: 404, message: "Aucun livre trouvé avec cet ID" };
+        }
+        return book;
     }
 }
