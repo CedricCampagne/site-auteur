80ffb44e861e382fd2b9ced40a58db2833d0fbdb
CedricCampagne <ced.campagne@gmail.com>
2026-01-23 12:21:04 +0100

    feat(errors): add global error handler and refactor AuthController to use next(error)

--- api/src/controllers/AuthController.ts
+++ api/src/controllers/AuthController.ts
@@ -1,22 +1,24 @@
 import { AuthServices } from "../services/authService";
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 
 export class AuthController {
-    static async register(req: Request, res: Response) {
+    static async register(req: Request, res: Response, next: NextFunction) {
         try {
             const result = await AuthServices.registerUser(req.body);
             return res.status(201).json(result);
         } catch (error: any) {
-            return res.status(400).json({message : error.message});
+            // return res.status(400).json({message : error.message});
+            next(error);
         }
     }
 
-    static async login(req: Request, res: Response) {
+    static async login(req: Request, res: Response, next: NextFunction) {
         try {
             const result = await AuthServices.loginUser(req.body);
             return res.status(200).json(result);
         } catch (error: any) {
-            return res.status(400).json({message : error.message});
+            // return res.status(400).json({message : error.message});
+            next(error);
         }
     }
 }
