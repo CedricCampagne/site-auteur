4648949b92832cc326c3a50ede658b6d7e7ca201
CedricCampagne <ced.campagne@gmail.com>
2026-01-23 14:31:54 +0100

    refactor(chronicles): update service, controller and routes to use global error handler

--- api/src/controllers/ChroniclesController.ts
+++ api/src/controllers/ChroniclesController.ts
@@ -1,77 +1,60 @@
-import { Request, Response } from "express";
-import { ChronicleService } from "../services/Chronicles";
+import { Request, Response, NextFunction } from "express";
+import { ChronicleService } from "../services/ChronicleService";
 
 export class ChroniclesController {
-    static async getAll(req: Request, res:Response) {
+    static async getAll(req: Request, res:Response, next: NextFunction) {
         try {
             const chronicles = await ChronicleService.getAllChronicles();
-            if(!chronicles){
-                return res.status(404).json({ error : "Chronicle not found"});
-            }
+
             return res.json(chronicles);
         } catch (error) {
-            console.error(error);
-            return res.status(500).json({error: "Internal server error"});
+            next(error);
         }
         
     }
 
-    static async getRandom(req: Request, res: Response) {
+    static async getRandom(req: Request, res: Response, next: NextFunction) {
         try {
             const chronicles = await ChronicleService.getRandom3Chronicles();
-            if(!chronicles){
-                return res.status(404).json({ error : "Chronicle not found"});
-            }
+
             return res.json(chronicles);
         } catch (error) {
-            console.error(error);
-            return res.status(500).json({error: "Internal server error"});            
+            next(error);       
         }
       
     }
 
-    static async getLatest3(req: Request, res: Response) {
+    static async getLatest3(req: Request, res: Response, next: NextFunction) {
         try {
-            const chronicles = await ChronicleService.getLatest3tChronicles();
-            if(!chronicles){
-                return res.status(404).json({ error : "Chronicle not found"});
-            }            
+            const chronicles = await ChronicleService.getLatest3Chronicles();
+
             return res.json(chronicles); 
         } catch (error) {
-            console.error(error);
-            return res.status(500).json({error: "Internal server error"});            
+            next(error);
         }
     }
 
-    static async getSlug (req: Request, res: Response) {
+    static async getSlug (req: Request, res: Response, next: NextFunction) {
         try {
             // const slug = req.params.slug;
-        const { slug } = req.params;
-        const chronicle = await ChronicleService.getBySlug(slug);
+            const { slug } = req.params;
+            const chronicle = await ChronicleService.getBySlug(slug);
 
-        if(!chronicle){
-            return res.status(404).json({ error : "Chronicle not found"});
-        }
-        return res.json(chronicle);
+            return res.json(chronicle);
         } catch (error) {
-            console.error(error);
-            return res.status(500).json({error: "Internal server error"});           
+            next(error);
         }
         
     }
 
-    static async getById (req: Request, res: Response) {
+    static async getById (req: Request, res: Response, next: NextFunction) {
         try {
             const id = Number(req.params.id);
             const chronicle = await ChronicleService.getById(id);
 
-            if(!chronicle) {
-                return res.status(404).json('Chronicle not found');
-            }
-            res.json(chronicle);
+            return res.json(chronicle);
         } catch (error) {
-            console.error(error);
-            return res.status(500).json({error: "Internal server error"});
+            next(error);
         }
     }
 }
