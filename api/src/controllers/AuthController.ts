74af78e9dcd1d46185b698469d30c06ae634506f
CedricCampagne <ced.campagne@gmail.com>
2026-01-29 14:20:12 +0100

    feat(auth): store JWT in httpOnly cookie in AuthController

--- api/src/controllers/AuthController.ts
+++ api/src/controllers/AuthController.ts
@@ -4,20 +4,36 @@ import { Request, Response, NextFunction } from "express";
 export class AuthController {
     static async register(req: Request, res: Response, next: NextFunction) {
         try {
-            const result = await AuthServices.registerUser(req.body);
-            return res.status(201).json(result);
+            const  {user, token} = await AuthServices.registerUser(req.body);
+
+            res
+                .cookie("token", token, {
+                    httpOnly: true,
+                    secure: false,
+                    sameSite: "strict",
+                    maxAge: 1*60*60*1000 // 1h <=> 3 600 000ms
+                })
+                .status(201)
+                .json({user});
         } catch (error: any) {
-            // return res.status(400).json({message : error.message});
             next(error);
         }
     }
 
     static async login(req: Request, res: Response, next: NextFunction) {
         try {
-            const result = await AuthServices.loginUser(req.body);
-            return res.status(200).json(result);
+            const {user, token} = await AuthServices.loginUser(req.body);
+
+            res
+                .cookie("token", token, {
+                    httpOnly: true,
+                    secure: false,
+                    sameSite: "strict",
+                    maxAge: 1*60*60*1000 // 1h <=> 3 600 000ms
+                })
+                .status(201)
+                .json({user});
         } catch (error: any) {
-            // return res.status(400).json({message : error.message});
             next(error);
         }
     }
