4b3301adfbdd15d0986940897f3d4f445828e3a0
CedricCampagne <ced.campagne@gmail.com>
2026-01-29 11:56:39 +0100

    refactor(errors): introduce HttpError class and update errorHandler to use custom HTTP errors

--- api/src/middleware/errorHandler.ts
+++ api/src/middleware/errorHandler.ts
@@ -1,9 +1,11 @@
 import { Request, Response, NextFunction } from "express";
+import { HttpError } from "../errors/HttpError";
+
 
 export function errorHandler(err: any, req: Request, res: Response, next: NextFunction) {
     console.error("Global Error Handler:", err);
 
-    const status = err.status || 500;
+    const status:number = (err instanceof HttpError) ? err.statusCode : 500;
 
     res.status(status).json({message: err.message || "Internal server error"});
 }
