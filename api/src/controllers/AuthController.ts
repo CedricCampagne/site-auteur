ee308a27948d65eac51bd58da131ca2023eafa51
CedricCampagne <ced.campagne@gmail.com>
2026-01-30 09:06:39 +0100

    feat(auth): add /auth/me route to return user from JWT cookie

--- api/src/controllers/AuthController.ts
+++ api/src/controllers/AuthController.ts
@@ -1,5 +1,8 @@
+import { error } from "node:console";
 import { AuthServices } from "../services/authService";
 import { Request, Response, NextFunction } from "express";
+import { HttpError } from "../errors/HttpError";
+import { verifyToken } from "../utils/jwt";
 
 export class AuthController {
     static async register(req: Request, res: Response, next: NextFunction) {
@@ -34,6 +37,26 @@ export class AuthController {
                 .status(201)
                 .json({user});
         } catch (error: any) {
+            next(error);
+        }
+    }
+
+    static async me(req: Request, res: Response, next: NextFunction){
+        try {
+            const token = req.cookies.token;
+    
+            if (!token){
+                throw new HttpError(401, "Non authetifié");
+            }
+            // renvoie payload si ok sinon null
+            const payload = verifyToken(token);
+            
+            if(!payload){
+                throw new HttpError(401, "Token invalide ou expiré");
+            }
+            
+            res.status(200).json({ user: payload });
+        } catch (error) {
             next(error);
         }
     }
