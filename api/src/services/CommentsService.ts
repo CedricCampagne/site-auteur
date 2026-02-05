240da0080ecca417d29e2eb18a21c53c451373d9
CedricCampagne <ced.campagne@gmail.com>
2026-02-05 11:20:07 +0100

    feat(comments): add CommentService and CommentController with getCommentsByChronicleId
    - Implemented service method to fetch comments by chronicle_id
    - Added controller endpoint handler
    - Included user join with selected attributes
    - Prepared route integration for comments retrieval

---
+++ api/src/services/CommentsService.ts
@@ -0,0 +1,18 @@
+import { Comment } from "../models/Comment";
+import { User } from "../models/User";
+
+export class CommentService{
+    static async getCommentsByChronicleId(chronicle_id: number){
+        const comments = await Comment.findAll({
+            where: {chronicle_id},
+            include: [
+                {
+                    model: User,
+                    attributes : ["id_user", "username"]
+                }
+            ],
+            order: [["created_at", "ASC"]]
+        })
+        return comments;
+    };
+}
