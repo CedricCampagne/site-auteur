de53d73f11b37b72b45e9f5ba5297636a3357f35
CedricCampagne <ced.campagne@gmail.com>
2026-02-18 09:59:02 +0100

    feat(admin-comments-filter): add users & chronicles to load + add dynamic filtering
    - Fetch users and chronicles in admin comments load function
    - Add user and chronicle <select> filters in UI
    - Implement combined dynamic filtering (user and/or chronicle) without page reload
    - Update comments list to use filteredComments
    - Improve admin UX with instant client-side filtering

--- client/src/routes/admin/comments/+page.server.ts
+++ client/src/routes/admin/comments/+page.server.ts
@@ -8,7 +8,22 @@ export const load : PageServerLoad = async ({fetch}) => {
     if (!res.ok) {
         throw new Error("Impossible de récupérer les commentaires");
     }
-
     const comments = await res.json();
-    return { comments };
+
+    const res2 = await fetch(`${import.meta.env.VITE_API_URL}/admin/users`,{
+        credentials: "include"
+    });
+
+    if (!res2.ok) {
+        throw new Error("Impossible de récupérer les utilisateurs");
+    }
+    const users = await res2.json();
+
+    const res3 = await fetch(`${import.meta.env.VITE_API_URL}/admin/chronicles`,
+        { credentials: "include"
+        });
+
+    const chronicles = await res3.json();
+    
+    return { comments, users, chronicles };
 }
