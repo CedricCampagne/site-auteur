7638678195ba052a3ed5d8947fb39f1c9cbe3779
CedricCampagne <ced.campagne@gmail.com>
2026-02-23 11:22:39 +0100

    feat: integrate sendResponse across backend and update front consumption
    - added sendResponse utility for unified API responses
    - updated global errorHandler to use sendResponse
    - refactored BooksController to return standardized responses
    - updated front-end load functions to read  from API responses

--- api/src/utils/sendResponse.ts
+++ api/src/utils/sendResponse.ts
@@ -1,21 +1,16 @@
-import { Response } from "express";
+import type { Response } from "express";
 
 export function sendResponse<T>(
-    res: Response,
-    type : "success" | "fail",
-    payload: T | string,
-    message?: string
-){
-    if(type === "success"){
-        return res.json({
-            type,
-            data: typeof payload === "string" ? undefined : payload,
-            message: typeof payload === "string" ? payload : message
-        });
-    }
-
-    return res.json({
-        type,
-        message: typeof payload === "string" ? payload : message
-    });
+  res: Response,
+  status: number,
+  type: "success" | "fail",
+  message: string,
+  data?: T
+) {
+  return res.status(status).json({
+    type,
+    message,
+    // si 1ere partie est true renvoie la seconde data : data
+    ...(data !== undefined && { data })
+  });
 }
