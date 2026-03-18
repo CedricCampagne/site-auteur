95212142d6327ffcc31de58143f986458ac2a6c0
CedricCampagne <ced.campagne@gmail.com>
2026-03-18 10:01:36 +0100

    chore: add a new book entry to data

--- api/src/data/dataBooks.ts
+++ api/src/data/dataBooks.ts
@@ -90,5 +90,49 @@ export const booksData = [
         genre: "Thriller",
         cover_url: "/images/legitimite-du-crime.png",
         is_active: false
+    },
+    {
+        title: "Dans mes veines (ancien IVM)",
+        author: " Katia Campagne",
+        summary: `
+        Les fractures d'enfance ne guerissent jamais vraiment. Elles s’insinuent dans les veines, dans le corps et dans l’esprit, et murmurent dans le silence des nuits.
+
+        Eléane avait six ans lorsqu’elle a fui la Nouvelle-Ecosse.
+        Une fuite improvisée, brutale, avec sa mère prête à tout pour la protéger d’une secte où les hommes se prennent pour des dieux et où les petites filles sont promises au sang.
+
+        Des années plus tard, dans le Vercors, la vie semble avoir repris son cours. Nouvelle langue, nouvelles règles, apparence de tranquillité.
+
+        Mais certains souvenirs ne meurent jamais. Et quand des meurtres violents secouent la région, les fractures du passé refont surface. Chaque geste, chaque silence, chaque regard devient un écho de cette nuit qui a changé sa vie.
+
+        Et si certaines fuites étaient impossibles ?
+        `,
+        excerpt: `
+        Il parait que quelque soit la profondeur de l’obscurité, une seule lueur permet de la faire disparaître. Il faut croire que mon obscurité devait être vraiment trop profonde.
+
+        Je me rappelle le jour où c’est arrivé. Nous habitions à Dartmouth en ce temps-là, en Nouvelle-Ecosse, année 2013, le mois de novembre pour être exact. C’était un vendredi. L’heure je ne m’en souviens pas trop, je ne savais pas la lire à l’époque, mais il faisait nuit. Je me rappelle que la pluie frappait très fort contre le toit de notre petite maison à la façade bleue.  Dans King Street, les maisons étaient toutes peintes d’une couleur différente. Les gens la surnommaient la rue arc-en-ciel et pensaient qu’il devait être très agréable d’y vivre. La nôtre était d’un bleu pastel, hésitant entre un ciel d’août et une mer calme de décembre suivant que le soleil caresse ses murs ou non. Nos voisins étaient une maison jaune et une rose. Je n’aurais pas aimé habiter dans une maison rose.
+
+        Les bourrasques de vent gémissaient contre les volets clos, faisant craquer atrocement le bois.  J’avais l’impression qu’il nous suppliait de le laisser entrer, pour échapper à l’enfer qui se déchaînait dans la rue. Il ne savait pas, le pauvre, que l’enfer vivait chez nous. Ça faisait trois semaines déjà qu’il pleuvait sans discontinuer. Les caniveaux étaient arrivés à saturation, dégorgeaient sur le bitume une eau noire alarmante. L’eau des lacs alentour, Maynard, Oat Hill et Sullivan Pond venait s’étaler sur les pelouses des parcs habituellement si entretenues et qui prenaient désormais l’allure de marécages boueux. Un petit air des bayous de Louisiane sans le folklore ni les alligators. Sans les touristes, ni la fête. Sans rien pour ramener notre moral à l’équilibre.
+
+        J’étais là-haut. En train de regarder la tempête par le carreau de l'œil de bœuf. Enfant, j’aimais bien ce nom qu’on donnait à la fenêtre du grenier. Regarder par l'œil de bœuf ne signifiait-il pas que j’étais à l’intérieur de la bête ? Ça donnait à l’endroit une dimension protectrice. Je me rend compte maintenant à quel point c’était stupide.
+
+        Au moment où les phares de la voiture ont balayé la façade, une nouvelle bourrasque s’est accrochée au carreau. Le joint d’isolation n’avait pas résisté au temps et l’ampoule dénudée au plafond  s’est agitée. Je ne sais pas si on la voyait de l’extérieur, il aurait fallu lever la tête et braver les rafales de pluies pour ça, mais j’ai posé la main dessus. Au  cas où. Et je suis descendue de mon observatoire aussi ; une chaise en bois que j’avais posée sur le plateau d’un bureau. Etrangement j’avais peur de tout en ce temps là, mais pas de mes perchoirs brinquebalants. J’ai éteint la lumière - je n’avais pas non plus peur du noir - et je me suis collée contre le mur le plus proche. Entre un carton plein de livres, une pile de journaux aux caractères incompréhensibles et des vieux meubles cassés abandonnés là. On avait ça en commun. La cassure et l’abandon.
+
+        La tempête éclairait suffisamment la pièce pour me permettre de voir les traces de boue sèche qui s’étalaient de mon promontoir jusqu’à ma couche sur le sol. Les mêmes que celles qui m’avaient valu de me retrouver là. Que maman avait déjà dû nettoyer. Depuis combien de temps j’étais là ? Trois ? Quatre heures ? Suffisamment de temps en tout cas pour qu’elle nettoie tout. Les traces de boue, les débris de verre, un nouveau morceau de mon enfance qui s’était fracassé au sol. Suffisamment de temps pour que mon estomac réclame et puis comprenne qu’il avait largement dépassé l’heure du repas et que c’était trop tard à présent.
+
+        Je n’ai pas entendu les portes de la voiture claquer, ni la porte d’entrée s’ouvrir. Mais j’ai su qu’il était là. Il y avait toujours une certaine vibration dans l’air quand il était dans la maison. Quelque chose d’électrique qui venaient secouer les ions de notre organisme. Instinctivement, nous étions sur nos gardes. Toujours. Tout le temps. Toujours quand j’étais là-haut et qu’il revenait de ses réunions. Je me suis frottée le nez pour ne pas éternuer, l’odeur âcre et poussiéreuse des vieux journaux me chatouillait, mais il ne fallait pas qu’il m’entende. Une bête vibrait en lui ces soirs-là, pire que le boeuf gigantesque dans lequel j'imaginais me trouver et d’effrayant mon père devenait monstrueux, sauvage. Son regard abyssal semblait alors pouvoir pénétrer en moi. Ses pupilles élargies ne reflétaient que l’obscurité d’un gouffre sans fond dans lequel, j’en étais persuadée, il aurait pu m’aspirer goulûment.  Les mots roulaient sur sa langue du tréfond de sa gorge et frottaient sur ma nuque leurs petits aiguillons acérés. J’étais pétrifiée, physiquement.
+
+        La tempête s’est arrêtée d’un seul coup, comme si, elle-aussi, retenait son souffle en attendant l’issue de cette fin de soirée. Il n’y avait que deux options possibles : soit il montait me voir, soit il s’installait devant un match avec un pack de six. Le soulagement était alors si intense qu’il en devenait physiquement douloureux. Il m’étreignait le coeur avec une telle intensité qu’il aurait pu me tuer sur place. Assez ironique quand on y pense.
+
+        Nous avons attendu la tempête et moi, il n’est pas venu. C’est Allan qui est venu à sa place pour me délivrer. Deux ans d’écart et pourtant si vieux. Il était tout à la fois mon frère. Un père, une mère, un médecin, l’ancre à laquelle je m’accrochais sans cesse. Je me rappelle que ce fameux soir, une odeur étrange, quelque chose d’acide, flottait autour de lui. Il avait une urgence dans le regard. Une angoisse. Mais je ne l’ai pas compris.
+        
+        Je n’ai compris que bien trop tard, des années plus tard en vérité, que c’est cet instant qui avait tout déterminé. Ce que je suis devenue et toute cette histoire. C’est à cet instant, dans ce regard que tout s’est joué. Je me souviens de tout : de l’odeur rance et confinée du grenier, du silence engrangé par la peur, de cette démangeaison sur mon poignet à cause d’une piqûre de moustique.  C’est amusant que je me souvienne encore de ces détails futiles.
+
+        Il parait que lorsque la mort approche, le film de notre vie se déroule sous nos yeux. Ça doit être à cause de ça que je me souviens de tout. Parce que j’ai le canon d’une arme  sous les yeux.
+        `,
+        published_at: new Date("2015-07-01"),
+        publisher: "autoédition",
+        genre: "Thriller",
+        cover_url: "/images/dans-mes-veines.png",
+        is_active: true
     }
 ];
