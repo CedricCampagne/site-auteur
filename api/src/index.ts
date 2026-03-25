5a69b71e495eaa1df44196e963e91c577f33272d
CedricCampagne <ced.campagne@gmail.com>
2026-03-25 11:11:22 +0100

    Resolve merge conflicts and clean index.ts & database.ts

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,14 +1,5 @@
 import "reflect-metadata";
 import dotenv from "dotenv";
-<<<<<<< HEAD
-dotenv.config({
-    path: `.env.${process.env.NODE_ENV || "development"}`
-});
-
-import mainRouter from "./routes/index.routes";
-
-import { sequelize } from "./config/database";
-=======
 
 if (process.env.NODE_ENV !== "production") {
   dotenv.config({
@@ -16,7 +7,6 @@ if (process.env.NODE_ENV !== "production") {
   });
 }
 
->>>>>>> prep/db-prod
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
@@ -28,17 +18,6 @@ import { sanitizeBody } from "./middleware/sanitize";
 import { errorHandler } from "./middleware/errorHandler";
 
 const app = express();
-<<<<<<< HEAD
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials:true
-}));
-
-app.use(helmet({
-    crossOriginResourcePolicy: false,
-    crossOriginOpenerPolicy: false
-}));
-=======
 
 app.use(cors({
   origin: 'http://localhost:5173',
@@ -50,23 +29,10 @@ app.use(helmet({
   crossOriginOpenerPolicy: false
 }));
 
->>>>>>> prep/db-prod
 app.use(express.json());
 app.use(cookieParser());
 app.use(sanitizeBody);
 
-<<<<<<< HEAD
-
-// test de co client sequelize
-sequelize.authenticate()
-.then(()=> console.log('Connection a pg via sequelize-ts ok'))
-.catch((err=>console.error('Erreur de co a la db', err)));
-
-app.use("/api", mainRouter);
-app.use(errorHandler);
-
-app.listen(process.env.PORT, ()=>console.log('API is running on http://localhost:3000'));
-=======
 app.use("/api", mainRouter);
 app.use(errorHandler);
 
@@ -81,4 +47,3 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`API running on port ${PORT}`));
->>>>>>> prep/db-prod
