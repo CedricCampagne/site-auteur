003ecc685936c64c81f8db2a09352e0a46f1d8bc
CedricCampagne <ced.campagne@gmail.com>
2026-03-26 10:00:44 +0100

    fix: cors and cookies cross-domain for front Vercel

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,10 +1,13 @@
 import "reflect-metadata";
 import dotenv from "dotenv";
 
+// Charger le .env selon NODE_ENV
 if (process.env.NODE_ENV !== "production") {
   dotenv.config({
     path: `.env.${process.env.NODE_ENV || "development"}`
   });
+} else {
+  dotenv.config({ path: `.env.production` });
 }
 
 import express from "express";
@@ -17,15 +20,18 @@ import { sequelize } from "./config/database";
 import { sanitizeBody } from "./middleware/sanitize";
 import { errorHandler } from "./middleware/errorHandler";
 
-
 import { User } from "./models/User";
 import { Book } from "./models/Book";
 import { Chronicle } from "./models/Chronicle";
+
 const app = express();
 
+const isProd = process.env.NODE_ENV === "production";
+
 app.use(cors({
-  // origin: 'http://localhost:5173',
-  origin: true,
+  origin: isProd
+    ? ['https://site-auteur-ashen.vercel.app']
+    : 'http://localhost:5173',     
   credentials: true
 }));
 
@@ -41,6 +47,7 @@ app.use(sanitizeBody);
 app.use("/api", mainRouter);
 app.use(errorHandler);
 
+
 (async () => {
   try {
     await sequelize.authenticate();
@@ -50,7 +57,7 @@ app.use(errorHandler);
   }
 })();
 
-// test staging 
+// test staging
 app.get("/debug", async (req, res) => {
   try {
     const users = await User.findAll();
